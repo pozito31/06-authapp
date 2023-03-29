ceb95f973d0c168c1dd02a1eb42432b68a281515
pozito31 <jessicapozo198715@gmail.com>
2023-03-29 16:10:06 +0200

    subir aplicacione a github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/protegida/protegida.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protegida',
+  templateUrl: './protegida.component.html',
+  styleUrls: ['./protegida.component.css']
+})
+export class ProtegidaComponent {
+
+}
